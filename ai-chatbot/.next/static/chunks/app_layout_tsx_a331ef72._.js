3c997be33f5f9c728cec891ec082ce1fb408c1d8
Jason Kneen <jason.kneen@bouncingfish.com>
2025-03-31 21:26:36 +0100

    Add ai-chatbot/.next/static/chunks/app_layout_tsx_a331ef72._.js 1743452796486

---
+++ ai-chatbot/.next/static/chunks/app_layout_tsx_a331ef72._.js
@@ -0,0 +1,12 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/app_layout_tsx_a331ef72._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/app_layout_tsx_a331ef72._.js",
+  "chunks": [
+    "static/chunks/_d6c0e298._.js",
+    "static/chunks/[root of the server]__c8d61cf1._.css"
+  ],
+  "source": "dynamic"
+});
