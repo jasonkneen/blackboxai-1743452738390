09025f9c71c4406ce6eb1557527f5c153b8e5f00
Jason Kneen <jason.kneen@bouncingfish.com>
2025-03-31 21:26:37 +0100

    Add ai-chatbot/.next/static/chunks/app_(auth)_login_page_tsx_9aad06d8._.js 1743452797345

---
+++ ai-chatbot/.next/static/chunks/app_(auth)_login_page_tsx_9aad06d8._.js
@@ -0,0 +1,13 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/app_(auth)_login_page_tsx_9aad06d8._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/app_(auth)_login_page_tsx_9aad06d8._.js",
+  "chunks": [
+    "static/chunks/[turbopack]_browser_dev_hmr-client_hmr-client_ts_92a24f49._.js",
+    "static/chunks/_718a05da._.js",
+    "static/chunks/node_modules__pnpm_e9fb0383._.js"
+  ],
+  "source": "dynamic"
+});
